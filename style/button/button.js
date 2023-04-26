d984d6a404a6e0e6a42c5e6c9e7157eee9f86ae2
GleIL <intoger3@gmail.com>
2023-04-26 19:49:23 +0300

    change background color

--- style/button/button.js
+++ style/button/button.js
@@ -3,4 +3,6 @@
 function AddClassCircle(){
     let circles = document.getElementById("circle");
     circles.classList.toggle("active");
+    let body = document.getElementById("body");
+    body.classList.toggle("active")
 }
