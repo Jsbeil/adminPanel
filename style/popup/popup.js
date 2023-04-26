bc3a7eefe3d2d454639dfb818f641911e148bc9b
GleIL <intoger3@gmail.com>
2023-04-26 19:11:07 +0300

    создание container

--- style/popup/popup.js
+++ style/popup/popup.js
@@ -3,8 +3,14 @@ function closeBg(){
     let bGpopup = document.getElementById("bg-popup");
     bGpopup.classList.add('disable');
 }
+function openBg(){
+    let container = document.getElementById("container-gl");
+    container.classList.add('active');
+}
 function closePopup(){
     let popup = document.getElementById("popupActive")
     popup.classList.remove('active');
     setTimeout(closeBg,1000);
+    setTimeout(openBg,1000);
 }
+
