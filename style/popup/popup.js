efdc5b8c0c49a0892e92ff1e52bba449e19a9a02
GleIL <intoger3@gmail.com>
2023-04-26 16:45:26 +0300

    fix popup

--- style/popup/popup.js
+++ style/popup/popup.js
@@ -1,8 +1,10 @@
 
-
-function closePopup(){
-    let popup = document.getElementById("popupActive")
+function closeBg(){
     let bGpopup = document.getElementById("bg-popup");
     bGpopup.classList.add('disable');
+}
+function closePopup(){
+    let popup = document.getElementById("popupActive")
     popup.classList.remove('active');
+    setTimeout(closeBg,1000);
 }
