ada0f5fc9e27246d254d753f6d8d07ddb3009ac6
GleIL <intoger3@gmail.com>
2023-04-29 00:27:25 +0300

    tusk 7 win

--- scripts/menu-creaetion.js
+++ scripts/menu-creaetion.js
@@ -1,12 +1,21 @@
+
+const imgaName = ["trv1.jpg","trv2.jpg","trv3.jpg","trv4.jpg","trv5.jpg","trv6.jpg"];
+
 function createArray(){
     let createpopup = document.getElementById("input-field").value;
     const menuItems = createpopup.split(", ")
     
     const menuList = document.createElement("ul");
-    menuItems.forEach(function(item) {
+    menuList.id = 'menu';
+    menuItems.forEach(function(item, index) {
         const menuItem =document.createElement("button");
         menuItem.classList.add("button-style-easy");
         menuItem.classList.add("button-array");
+        
+       menuItem.addEventListener("click", function(){
+            const image = document.getElementById("image");
+            image.src = "images/" + index +".jpg";
+       })
         let color = document.getElementById("head").value;
         menuItem.style.color = color;
         let color1 = document.getElementById("head1").value;
@@ -23,9 +32,12 @@ function createArray(){
         menuItem.textContent = item.trim();
         menuList.appendChild(menuItem);
     });
+
     const menuContainer = document.getElementById("menu-container");
     menuContainer.innerHTML = "";
     menuContainer.appendChild(menuList);
+    const menuCounter1 = document.getElementById("image-container");
+    menuCounter1.appendChild(button);
     
     console.log(menuItems);
 }
